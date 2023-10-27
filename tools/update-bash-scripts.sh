a59d9769ad873678bc2ecde96d474e92046335e5
Nikita Saltykov <greenders87@gmail.com>
2023-10-28 00:11:40 +0200

    Update

--- tools/update-bash-scripts.sh
+++ tools/update-bash-scripts.sh
@@ -1,25 +1,27 @@
-#!/bin/sh
-# Author: Nikita Saltykov
+# #!/bin/sh
+# # Author: Nikita Saltykov
 
-# enable the "errexit" option
-set -e
+# # enable the "errexit" option
+# set -e
 
-# Make sure important variables exist if not already defined
-#
-# $USER is defined by login(1) which is not always executed (e.g. containers)
-# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
-USER=${USER:-$(id -u -n)}
-# $HOME is defined at the time of login, but it could be unset. If it is unset,
-# a tilde by itself (~) will not be expanded to the current user's home directory.
-# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
-HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
-# macOS does not have getent, but this works even if $HOME is unset
-HOME="${HOME:-$(eval echo ~$USER)}"
+# # Make sure important variables exist if not already defined
+# #
+# # $USER is defined by login(1) which is not always executed (e.g. containers)
+# # POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
+# USER=${USER:-$(id -u -n)}
+# # $HOME is defined at the time of login, but it could be unset. If it is unset,
+# # a tilde by itself (~) will not be expanded to the current user's home directory.
+# # POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
+# HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
+# # macOS does not have getent, but this works even if $HOME is unset
+# HOME="${HOME:-$(eval echo ~$USER)}"
 
-# Default settings
-REPO=${REPO:-doubleniki/bash-scripts}
-REMOTE=${REMOTE:-https://github.com/${REPO}.git}
-BRANCH=${BRANCH:-master}
+# # Default settings
+# REPO=${REPO:-doubleniki/bash-scripts}
+# REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+# BRANCH=${BRANCH:-master}
+
+source ./base.sh
 
 # check if git is installed
 if ! command_exists git; then
